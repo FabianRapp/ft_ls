2ab7d323565772ea78f74e8a070d2abc566b0d5b
Fabian Rapp <frapp@2-D-9.42heilbronn.de>
2026-05-31 15:27:59 +0200

    a

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <dirent.h>
 #include <errno.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 
 //-l     use a long listing format
@@ -65,27 +68,115 @@ t_args_type args_type(const char *arg) {
 	return PATH;
 }
 
-void handle_dir(char *path) {
-	DIR *dir = opendir(paths[i]);
+void print_info(const char *path, t_modes modes) {
+	printf("%s ", path);
+}
+
+char *get_sub_dir_path(const char *path, const char *sub_name) {
+	char *sub_dir;
+	int path_len = ft_strlen(path);
+	if (path[path_len - 1] == '/') {
+		sub_dir = ft_strjoin(path, sub_name);
+	} else {
+		char *tmp = ft_strjoin(path, "/");
+		if (!tmp) {
+			ft_fprintf(2, "Malloc error\n");
+			return NULL;
+		}
+		sub_dir = ft_strjoin(tmp, sub_name);
+		free(tmp);
+	}
+	if (!sub_dir) {
+		ft_fprintf(2, "Malloc error\n");
+		return NULL;
+	}
+	return sub_dir;
+}
+
+//todo
+void handle_dir(const char *path, t_modes modes, char ***paths, int *path_count) {
+	DIR *dir = opendir(path);
 	if (dir == NULL) {
 		switch (errno) {
-			case (EACCES): break ;// Permission denied.
+			case (EACCES): break ;// Permission denied/.
 			case (EBADF): break ;//  fd is not a valid file descriptor opened for reading.
 			case (EMFILE): break ;// The per-process limit on the number of open file descriptors has been reached.
 			case (ENFILE): break ;// The system-wide limit on the total number of open files has been reached.
 			case (ENOENT): break ;// Directory does not exist, or name is an empty string.
 			case (ENOMEM): break ;// Insufficient memory to complete the operation.
 			case (ENOTDIR): assert(0);//name is not a directory.
-			}
 		}
-		closedir(dir);
+	}
+	if (modes.recursive) {
+		ft_printf("%s:\n", path);
+	}
+	//todo: first save all the paths, sort them case sensitive and then work on them
+	for (struct dirent *content = readdir(dir); content != NULL; content = readdir(dir)) {
+		if (content->d_name[0] == '.' && !modes.all) {
+			continue ;
+		}
+		print_info(content->d_name, modes);
+		if (modes.recursive && content->d_type == DT_DIR
+			&& ft_strcmp(content->d_name, ".") && ft_strcmp(content->d_name, "..")) {
+			char *rec_dir = get_sub_dir_path(path, content->d_name);
+			if (!rec_dir) {
+				return ;
+			}
+			dyn_arr_add_save((void**)paths, (void*)(&rec_dir), (*path_count)++);
+		}
+	}
+	closedir(dir);
+	if (modes.recursive) {
+		ft_printf("\n");
+	}
+}
+
+void handle_path(const char *path, t_modes modes, char ***paths, int *path_count) {
+	struct stat stat;
+	if (lstat(path, &stat) < 0) {
+		// todo: error
+		ft_fprintf(2, "Error with path: %s: %s\n", path, strerror(errno));
+		return ;
+	}
+	unsigned type_bits = stat.st_mode & S_IFMT;
+
+    if (type_bits == S_IFSOCK) { //  0140000   socket
+	}
+	if (type_bits == S_IFLNK) { //symbolic link
+	}
+	if (type_bits == S_IFREG) { //regular file
+	}
+	if (type_bits == S_IFBLK) { //block device
+	}
+	if (type_bits == S_IFDIR) { //directory
+		handle_dir(path, modes, paths, path_count);
+	}
+	if (type_bits == S_IFCHR) { //character device
+	}
+	if (type_bits == S_IFIFO) { //FIFO
+	}
+
+	unsigned permisson_bits = stat.st_mode & 0777;
+
+mode_t    st_mode;        /* File type and mode */
+nlink_t   st_nlink;       /* Number of hard links */
+uid_t     st_uid;         /* User ID of owner */
+gid_t     st_gid;         /* Group ID of owner */
+off_t     st_size;        /* Total size, in bytes */
+blksize_t st_blksize;     /* Block size for filesystem I/O */
+blkcnt_t  st_blocks;      /* Number of 512B blocks allocated */
 }
 
 int main(int ac, char **av) {
-	char **paths =dyn_arr_init(sizeof(char *), 0);
+	char **paths = dyn_arr_init(sizeof(char *), 0);
+	if (!paths) {
+		ft_printf("Malloc error\n");
+		return 1;
+	}
 	int path_count = 0;
 	t_modes modes = {0};
-	ft_printf("hello world\n");
+	const char *cur_dir = ".";
+
 	for (int i = 1; i < ac; i++) {
 		const t_args_type arg_type = args_type(av[i]);
 		set_mode(&modes, arg_type);
@@ -94,13 +185,23 @@ int main(int ac, char **av) {
 		}
 		ft_printf("args type %s\n", args_type_to_str(arg_type));
 	}
-	ft_printf("path count: %d\n", path_count);
+
+	//ft_printf("path count: %d\n", path_count);
+
+	if (path_count == 0) {
+		dyn_arr_add_save((void**)(&paths), (void*)(&cur_dir), path_count++);
+	}
+	const int non_recurisve_path_count = path_count;
 	for (int i = 0 ; i < path_count; i++) {
-		ft_printf("Working on path %s..\n", paths[i]);
-
-
+		handle_path(paths[i], modes, &paths, &path_count);
+	}
+	for (int i = non_recurisve_path_count; i < path_count; i++) {
+		free(paths[i]);
 	}
 
 	dyn_arr_free((void **)(&paths));
+	if (modes.recursive) {
+		ft_printf("\n");
+	}
 	return 0;
 }
